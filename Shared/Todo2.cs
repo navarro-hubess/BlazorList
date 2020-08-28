cafe4984a75fbf28c89ef0c21acd7e1d0cca8902
navarrowebapi <navarro.fabio@gmail.com>
2020-08-27 22:46:58 -0300

    lista com check

---
+++ Shared/Todo2.cs
@@ -0,0 +1,11 @@
+namespace blazorizeApp.Shared{
+
+    public class Todo2
+    {
+        public string Title { get; set; }
+        public bool Done { get; set; }
+        
+    }
+
+
+}
---
+++ Shared/TodoEntity.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace blazorizeApp.Shared
+{
+    public class TodoEntity
+    {
+        public string Title { get; set; }
+        public bool Done { get; set; }
+
+    }
+}
